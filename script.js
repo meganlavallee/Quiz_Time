f6798bbff618c4197b460c63becf0148303c36f1
meganlavallee <73404164+meganlavallee@users.noreply.github.com>
2020-11-09 10:37:40 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const startBtn = document.querySelector("#start");
+const startPrompt = document.querySelector("#start-prompt");
+const questionContainer = document.querySelector("#modern-photo")
+
+//Click Event Handler
+startBtn.addEventListener("click", function (e){});
+
+//Start Timer
+
+
+//Hide Start Prompt
+startPrompt.style.display = "none";
+
+//Show Question Container
+questionContainer.style.display ="block";
