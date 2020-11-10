4f09fd8e82db9535f689c0fee61e8c0c0ba8b1ba
meganlavallee <73404164+meganlavallee@users.noreply.github.com>
2020-11-10 16:12:41 -0700

    submitting

--- script.js
+++ script.js
@@ -3,13 +3,33 @@ const startPrompt = document.querySelector("#start-prompt");
 const questionContainer = document.querySelector("#modern-photo")
 
 //Click Event Handler
-startBtn.addEventListener("click", function (e){});
+startPrompt.addEventListener("click", function (e){});
 
 //Start Timer
 startTimer.addEventListener("click", start (e))
+
+const start = startTimer.now();
+setInterval(function() {
+    const delta = startTimer.now() - start; 
+    output(Math.floor(delta / 1000)); 
+    output(startTime().toUTCString());
+}, 1000);
 
 //Hide Start Prompt
 startPrompt.style.display = "none";
 
 //Show Question Container
 questionContainer.style.display ="block";
+
+<input type="start-prompt" name="ShowHide" onclick="ShowHidePrompt(this.value)" value="0" checked /> Show <br/>
+
+<input type="modern-photo" name="ShowHide" onclick="ShowHidePrompt(this.value)" value="1" /> Hide
+
+    function ShowHidePrompt(value)
+    {
+        if(parseInt(value) == 0)
+            document.getElementById("divPrompt").style.display="block";
+        else
+        document.getElementById("divPrompt").style.display="none";
+    }
+
