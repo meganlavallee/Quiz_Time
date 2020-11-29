cde83569e67fd487b6b6e7670d41b255983e1292
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-28 20:24:41 -0700

    new script page, adding form

---
+++ script2.js
@@ -0,0 +1,19 @@
+
+document.getElementById("form1").onsubmit=function() {
+       variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
+	   sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
+	   con = parseInt(document.querySelector('input[name = "con"]:checked').value);
+	   ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+	   
+	   
+	   result = variable + sub + con + ifstate;
+	   
+	document.getElementById("grade").innerHTML = result;
+	   
+
+
+return false; // required to not refresh the page; just leave this here
+} //this ends the submit function
+
+
+
