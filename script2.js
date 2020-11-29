702feecff47defb0628198eb2a137dab92d6d1f7
Megan Lavallee <megan.lavallee@yahoo.com>
2020-11-28 21:55:50 -0700

    functionality of webpage

--- script2.js
+++ script2.js
@@ -1,12 +1,19 @@
 
 document.getElementById("form1").onsubmit=function() {
-       variable = parseInt(document.querySelector('input[name = "variable"]:checked').value);
-	   sub = parseInt(document.querySelector('input[name = "sub"]:checked').value);
-	   con = parseInt(document.querySelector('input[name = "con"]:checked').value);
-	   ifstate = parseInt(document.querySelector('input[name = "ifstate"]:checked').value);
+
+       modern = parseInt(document.querySelector('input[name = "modern"]:checked').value);
+        
+       process = parseInt(document.querySelector('input[name = "process"]:checked').value);
+        
+       word = parseInt(document.querySelector('input[name = "word"]:checked').value);
+        
+       camera = parseInt(document.querySelector('input[name = "camera"]:checked').value);
+        
+       composition = parseInt(document.querySelector('input[name = "composiion"]:checked').value);
+        
 	   
 	   
-	   result = variable + sub + con + ifstate;
+	   result = modern + process + word + camera + composition;
 	   
 	document.getElementById("grade").innerHTML = result;
 	   
